90efdce70e36a69288cd75f78aafb742c8a4de05
mayli4 <sydsleeper@gmail.com>
2025-12-15 22:39:01 -0500

    feat: cmd buffer ext

---
+++ src/Magpie/Ext/CmdBufferExt.cs
@@ -0,0 +1,17 @@
+﻿using Auklet.Core;
+using Standard;
+using System.Numerics;
+
+namespace Magpie.Ext;
+
+public static class CmdBufferExt {
+    extension(CmdBuffer buf) {
+        public void SetViewport(Rectangle rect, float minDepth = 0f, float maxDepth = 1f) 
+            => buf.SetViewport(new Vector4(rect.X, rect.Y, rect.Width, rect.Height), minDepth, maxDepth);
+    }
+    
+    extension(CmdBuffer buf) {
+        public void SetScissor(Rectangle rect) 
+            => buf.SetScissor(new Rectangle(rect.X, rect.Y, rect.Width, rect.Height));
+    }
+}
